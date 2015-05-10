3ef4ff406a37aed1d7e4e7c7c4413f472a6341d6
Sonny Nguyen <phplaw@gmail.com>
2015-05-10 12:43:34 +0700

    Create plugin_native_session.php

---
+++ plugin_native_session.php
@@ -0,0 +1,17 @@
+<?php
+/**
+Turn on Native Session for wordpress site
+*/
+add_action('init', 'myStartSession', 1);
+add_action('wp_logout', 'myEndSession');
+add_action('wp_login', 'myEndSession');
+
+function myStartSession() {
+    if(!session_id()) {
+        session_start();
+    }
+}
+
+function myEndSession() {
+    session_destroy ();
+}
